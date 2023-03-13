ad0ab344a8702caec4bc1e58b177acd0197a9b66
Bakare Abiola <iamstarcode@gmail.com>
2023-03-13 07:45:25 +0100

    *

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { useState } from 'react';
+import { Database } from 'types/database';
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [supabaseClient] = useState(() =>
+    createBrowserSupabaseClient<Database>()
+  );
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
